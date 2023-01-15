c84f2d03569c65bc24fe6920912ba214ad9898d4
goyois <bau12288@gmail.com>
2023-01-16 01:29:37 +0900

    Commit Test

---
+++ src/test/java/MySQL/sqlEx/SqlExApplicationTests.java
@@ -0,0 +1,13 @@
+package MySQL.sqlEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
